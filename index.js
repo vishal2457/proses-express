eb81dca3110965162cc9fb3065b377c679ba93c9
Vishal Acharya <vishal.proses@gmail.com>
2021-10-14 19:18:20 +0530

    changes

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 'use strict';
+let proses = require('./lib/prosesExpress');
 
+let app = proses({encryption: "vishal", logging: true})
+let { successResponse, other } = proses.Response;
+
+
+app.listen()
+console.log(other("hi"), "this is other");
 module.exports = require('./lib/prosesExpress');
